0fec2158fb6f02fe216b5dc10a785fab031ed986
Randeer Lalanga <39120946+randeer@users.noreply.github.com>
2024-08-31 09:27:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,14 @@
+resource "random_string" "random" {
+  length           = 8
+  special          = true
+  override_special = "/@£$&%#!*"
+}
 
+resource "random_id" "id_random" {
+  byte_length = 8
+}
+
+resource "local_file" "random_file" {
+  filename = "${random_string.random}_${random_id.id_random}.txt"
+  content  = var.data
+}
