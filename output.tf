3c909f8d61c0fdb30b322a9129c5b7e4bf7682d1
Randeer Lalanga <39120946+randeer@users.noreply.github.com>
2024-10-11 21:43:13 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,11 +1,14 @@
 output "result" {
   value = "Your file is created."
+  description = "Output that your file is created"
 }
 
 output "file_name" {
   value = "Created file name is ${var.file_name} and it is ready to check."
+  description = "Output the file name of the file"
 }
 
 output "file_data" {
   value = "The file data: ${var.data}"
+  description = "Output the file content"
 }
